583fe5751e541bdedeead1d325d1f84822c08945
kevsonic <ktyler8@my.apsu.edu>
2024-12-05 14:14:44 -0600

    More style header changes

--- header.php
+++ header.php
@@ -10,8 +10,10 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/overstock-daily-deals/css/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> <!-- Import Google Font -->
     <title>Overstock Daily Deals</title>
 </head>
+
 <body>
     <header class="main-header">
         <div class="container">
