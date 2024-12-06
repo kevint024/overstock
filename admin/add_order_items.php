65216b317977dbb39e88677df8853acf6c646e1f
kevsonic <ktyler8@my.apsu.edu>
2024-12-05 20:48:13 -0600

    Fix stylesheet link in add order items page to ensure correct styling

--- admin/add_order_items.php
+++ admin/add_order_items.php
@@ -50,7 +50,7 @@ $result_products = $conn->query($sql_products);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Order Items</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <?php include __DIR__ . '/../header.php'; ?>
