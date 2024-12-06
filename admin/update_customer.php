dbe01c4e5550bb6073efc5ffcde8df491524f860
kevsonic <ktyler8@my.apsu.edu>
2024-12-05 20:49:13 -0600

    Fix stylesheet link in update customer page to ensure correct styling

--- admin/update_customer.php
+++ admin/update_customer.php
@@ -74,6 +74,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Customer</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>Update Customer</h1>
